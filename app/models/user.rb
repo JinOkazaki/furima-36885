69a8c5050ed258ece55c781cfbd56c744cfdefdc
JinOkazaki <o.jin19951212@icloud.com>
2021-09-22 17:36:18 +0900

    バリデーション設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Last name is invalid. Input full-width characters"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "First name is invalid. Input full-width characters"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "Last name kana is invalid. Input full-width katakana characters"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "First name kana is invalid. Input full-width katakana characrers"}
+    validates :encrypted_password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Password is invalid. Include both letters and numbers"}
+  end
 end
