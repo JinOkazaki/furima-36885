e7d107ef4c41f1b124081148ae82b1e46c16cbd2
JinOkazaki <o.jin19951212@icloud.com>
2021-09-22 21:51:19 +0900

    rspec,factorybot,fakerの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {'TARO'}
+    last_name {'山田'}
+    first_name {'太朗'}
+    last_name_kana {'ヤマダ'}
+    first_name_kana {'タロウ'}
+    birthday {'2000-12-12'}
+    email {'taro@test.jp'}
+    password {'taro000'}
+    password_confirmation {password}
+  end
+end
