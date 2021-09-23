4ab037d336afc5d676cdafeb07dac57236d470ad
JinOkazaki <o.jin19951212@icloud.com>
2021-09-23 16:41:05 +0900

    itemモデルにバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -8,4 +9,16 @@ class Item < ApplicationRecord
   belongs_to :Prefecture
   belongs_to :shopping_day
 
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :explanation
+    validates :category_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :status_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :delivery_fee_id, numericality: {other_that: 1, message: "can't be blank"}
+    validates :Prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :shopping_day_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :price, numericality: {in: 300..9999999,message: "is out of setting range"}
+                      format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+  end
 end
