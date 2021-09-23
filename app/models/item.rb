836715dadade6be4989fd3bcb3c25233e9f422aa
JinOkazaki <o.jin19951212@icloud.com>
2021-09-23 23:54:06 +0900

    販売手数料、販売利益の自動表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
-  belongs_to :Prefecture
+  belongs_to :prefecture
   belongs_to :shopping_day
 
   with_options presence: true do
@@ -15,9 +15,9 @@ class Item < ApplicationRecord
     validates :explanation
     validates :category_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :status_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :delivery_fee_id, numericality: {other_that: 1, message: "can't be blank"}
-    validates :Prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :delivery_fee_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :shopping_day_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :price, numericality: {in: 300..9999999,message: "is out of setting range"},format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
+    validates :price, numericality: {greater_than_or_equal_to:300, less_than_or_equal_to:9999999, message: "is out of setting range"},format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"}
   end
 end
