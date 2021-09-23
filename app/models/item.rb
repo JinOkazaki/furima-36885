8d8fa947d483b8d2d5cefcf07c06c18d80f6df45
JinOkazaki <o.jin19951212@icloud.com>
2021-09-23 15:52:57 +0900

    ActiveHashを利用する各モデルファイル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :Prefecture
+  belongs_to :shopping_day
+
 end
