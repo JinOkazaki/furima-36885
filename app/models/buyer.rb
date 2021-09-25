3a5e68d7faa48d8aa72a91285c0d732ac05696cc
JinOkazaki <o.jin19951212@icloud.com>
2021-09-25 14:16:05 +0900

    buyerモデルにバリデーション設定

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,9 @@
 class Buyer < ApplicationRecord
+  with_options presence: true do
+    validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :tell_number, format: {with: /\A[0-9]{10,11}\z/}
+  end
 end
